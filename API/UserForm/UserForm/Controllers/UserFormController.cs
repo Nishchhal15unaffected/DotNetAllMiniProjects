f68a2fa9de1f23eba2b0803c3671365c60596f7e
91626 <91626@LAPTOP-U7GEUGF8>
2025-01-19 17:29:14 +0530

    api project

---
+++ API/UserForm/UserForm/Controllers/UserFormController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserForm.Models;
+
+namespace UserForm.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class UserFormController : Controller
+    {
+        
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to my application";
+        }
+        [HttpGet ("UserName")]
+        public string GetUserName()
+        {
+            string userName = "Nishchhal";
+            return userName;
+        }
+        [HttpPost]
+        public string Post([FromBody]UserModel userModel)
+        {
+            if(userModel.FirstName != null && userModel.FirstName != string.Empty)
+            {
+                return "success";
+            }
+            return "Failed";
+        }
+    }
+}
---
+++ API/UserForm/UserForm/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UserForm.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Active { get; set; }
+        public string Address { get; set; }
+
+    }
+}
