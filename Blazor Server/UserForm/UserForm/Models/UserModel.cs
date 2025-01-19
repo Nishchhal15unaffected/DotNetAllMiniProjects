eed9f9808bc19ec87491eb6f79b58134e3839bc3
91626 <91626@LAPTOP-U7GEUGF8>
2025-01-19 18:56:28 +0530

    blazor project

---
+++ Blazor Server/UserForm/UserForm/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserForm.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Active { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
